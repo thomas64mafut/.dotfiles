b420ac3b61e1326d3bfecae313eb7480762aabae
thomas mafut <mafut50@gmail.com>
2023-03-06 22:14:15 -0300

    added fix for fedora installs

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+# added permissions into selinux for fedora (uncomment)
+sudo semanage fcontext -a -t etc_t '/nix/store/[^/]+/etc(/.*)?'
+sudo semanage fcontext -a -t lib_t '/nix/store/[^/]+/lib(/.*)?'
+sudo semanage fcontext -a -t systemd_unit_file_t '/nix/store/[^/]+/lib/systemd/system(/.*)?'
+sudo semanage fcontext -a -t man_t '/nix/store/[^/]+/man(/.*)?'
+sudo semanage fcontext -a -t bin_t '/nix/store/[^/]+/s?bin(/.*)?'
+sudo semanage fcontext -a -t usr_t '/nix/store/[^/]+/share(/.*)?'
+sudo semanage fcontext -a -t var_run_t '/nix/var/nix/daemon-socket(/.*)?'
+sudo semanage fcontext -a -t usr_t '/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
+
 # install nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
