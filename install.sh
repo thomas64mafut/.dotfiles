09acb2e67c111404d120d29f0d22e77979f148bd
thomas mafut <mafut50@protonmail.com>
2024-01-20 15:55:03 -0300

    final test

--- install.sh
+++ install.sh
@@ -48,13 +48,12 @@ git config --global user.email "$email"
 
 git config --global init.defaultBranch main
 
-echo \n\n
-echo 'RELOAD TERMINAL AND PASTE THE NEXT COMMANDS!!!!'
-echo \n\n
-echo 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher 
+echo "\n\n"
+echo "RELOAD TERMINAL AND PASTE THE NEXT COMMANDS!!!!\n"
+echo "echo 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher 
 fisher install jorgebucaran/nvm.fish
 fisher install jorgebucaran/autopair.fish
 fisher install lilyball/nix-env.fish
 fisher install jethrokuan/fzf
 fisher install jethrokuan/z
-fisher install rstacruz/fish-npm-global'
+fisher install rstacruz/fish-npm-global"
